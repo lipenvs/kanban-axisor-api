38bde99553d706401fbf89f8de8542a5efc5391b
Felipe Neves <rustuniverso@gmail.com>
2026-02-14 14:09:24 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
