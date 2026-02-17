65d9ba0d350a3aa42194e1007bfc04e3e826c929
Felipe Neves <rustuniverso@gmail.com>
2026-02-17 15:26:49 -0300

    feat: add E2E test for updating a project

---
+++ src/http/routes/update-project.spec.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from 'vitest'
+import { makeUser } from '../../tests/factories/make-user'
+import { randomUUIDv7 } from 'bun'
+import { makeProject } from '../../tests/factories/make-project'
+import { app } from '../../app'
+import { eq } from 'drizzle-orm'
+import { db } from '../../database/client'
+import { project } from '../../database/schema'
+
+describe('Update project', () => {
+  it('should update a project', async () => {
+    const { cookie, userId } = await makeUser()
+    const projectName = randomUUIDv7()
+    const projectCreated = await makeProject(userId, projectName)
+
+    const response = await app.handle(
+      new Request(`http://localhost/projects/${projectCreated.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: cookie,
+        },
+        body: JSON.stringify({
+          name: 'new name',
+        }),
+      })
+    )
+
+    expect(response.status).toBe(200)
+
+    const projectInDb = await db
+      .select()
+      .from(project)
+      .where(eq(project.id, projectCreated.id))
+
+    expect(projectInDb[0]).toEqual(
+      expect.objectContaining({
+        name: 'new name',
+      })
+    )
+  })
+})
