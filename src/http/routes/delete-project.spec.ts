81336fe9c83e17ac123ce1cc1a35b116ecd64ae0
Felipe Neves <rustuniverso@gmail.com>
2026-02-17 15:18:09 -0300

    test: add test for deleting a project

---
+++ src/http/routes/delete-project.spec.ts
@@ -0,0 +1,32 @@
+import { describe, it, expect } from 'vitest'
+import { app } from '../../app'
+import { makeUser } from '../../tests/factories/make-user'
+import { makeProject } from '../../tests/factories/make-project'
+import { randomUUIDv7 } from 'bun'
+import { eq } from 'drizzle-orm'
+import { db } from '../../database/client'
+import { project } from '../../database/schema'
+
+describe('Delete project', () => {
+  it('should delete a project', async () => {
+    const { cookie, userId } = await makeUser()
+    const projectName = randomUUIDv7()
+    const projectCreated = await makeProject(userId, projectName)
+
+    const response = await app.handle(
+      new Request(`http://localhost/projects/${projectCreated.id}`, {
+        method: 'DELETE',
+        headers: { Cookie: cookie },
+      })
+    )
+    
+    expect(response.status).toBe(200)
+
+    const projectInDb = await db
+      .select()
+      .from(project)
+      .where(eq(project.id, projectCreated.id))
+
+    expect(projectInDb[0]).toBeUndefined()
+  })
+})
