7d1a096eefed207482299a228a60194e8adf971b
Felipe Neves <rustuniverso@gmail.com>
2026-02-21 15:37:29 -0300

    feat: Refactor email verification logic to enable sending confirmation emails on sign-up

--- src/modules/better-auth/service.ts
+++ src/modules/better-auth/service.ts
@@ -3,13 +3,15 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { openAPI } from 'better-auth/plugins';
 import { db } from '../../database/client';
 import { env } from '../../env';
+import { resend } from '../../mail/client';
+import ConfirmAccountEmail from '../../mail/templates/confirm-account';
 
 export const auth = betterAuth({
   baseURL: env.BETTER_AUTH_URL,
   trustedOrigins: [env.FRONTEND_URL],
   basePath: '/auth',
   plugins: [openAPI()],
-	database: drizzleAdapter(db, {
+  database: drizzleAdapter(db, {
     provider: 'pg',
   }),
   advanced: {
@@ -29,16 +31,16 @@ export const auth = betterAuth({
   emailVerification: {
     sendOnSignUp: true,
     autoSignInAfterVerification: true,
-    sendVerificationEmail: async ({ user, url}) => {
-      // void resend.emails.send({
-      //   from: 'Kanban Axisor <axisor@universorust.com.br>',
-      //   to: user.email,
-      //   subject: 'Confirme sua conta no Kanban Axisor',
-      //   react: ConfirmAccountEmail({
-      //     username: user.name,
-      //     confirmationLink: url,
-      //   }),
-      // });
+    sendVerificationEmail: async ({ user, url }) => {
+      void resend.emails.send({
+        from: 'Kanban Axisor <axisor@universorust.com.br>',
+        to: user.email,
+        subject: 'Confirme sua conta no Kanban Axisor',
+        react: ConfirmAccountEmail({
+          username: user.name,
+          confirmationLink: url,
+        }),
+      });
     },
   },
   session: {
